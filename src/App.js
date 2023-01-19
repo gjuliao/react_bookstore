be9bcde31ed7fc53f38908723215d82827980924
gijoe2day <gjuliao32@hotmail.com>
2023-01-19 17:23:55 -0500

    Added book component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable no-unused-vars */
+import './index.css';
+import { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Book from './components/Book';
+import Form from './components/Form';
 
-function App() {
+const App = () => {
+  const [books, setBooks] = useState({
+    id: 1,
+    author: 'JK Rowlings',
+    title: 'Harry Potter',
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Book books={books} />} />
+          <Route path="/form" element={<Form />} />
+        </Routes>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/components/Book.js
@@ -0,0 +1,46 @@
+/* eslint-disable no-console, react/destructuring-assignment */
+import React from 'react';
+import propTypes from 'prop-types';
+
+const Book = (props) => {
+  const { author, title } = props.books;
+
+  return (
+    <>
+      <div>
+        <h4>Science Fiction</h4>
+        <h1>{title}</h1>
+        <h2>{author}</h2>
+        <ul className="book_button_list">
+          <li><a href="/">Comments</a></li>
+          <li><a href="/">Remove</a></li>
+          <li><a href="/">Edit</a></li>
+        </ul>
+      </div>
+      <div>
+        <div>
+          <h1>Image Icon</h1>
+          <h1>8%</h1>
+          <h4>Completed</h4>
+        </div>
+        <div>
+          <h4>Current Chapter</h4>
+          <h3>Chapter 3: A Lesson Learned</h3>
+          <button type="button">UPDATE PROGRESS</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Book;
+
+Book.defaultProps = {
+  books: null,
+  author: '',
+};
+
+Book.propTypes = {
+  books: propTypes.instanceOf(Object),
+  author: propTypes.string,
+};
