a42e355241eedd7c1f4ac84ff10d063bf1e95310
gijoe2day <gjuliao32@hotmail.com>
2023-01-23 18:50:22 -0500

    Finished navbar & card initial styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,12 +4,14 @@ import { NavLink } from 'react-router-dom';
 
 const Navbar = () => (
   <nav className="navbar">
-    <h1>Bookstore CMS</h1>
-    <ul className="nav_list">
-      <NavLink className="nav_link" to="/">Books</NavLink>
-      <NavLink to="/categories">Categories</NavLink>
-    </ul>
-    <div>User</div>
+    <div className="navbar_main">
+      <h1>Bookstore CMS</h1>
+      <ul className="nav_list">
+        <NavLink className="nav_link" to="/">Books</NavLink>
+        <NavLink to="/categories">Categories</NavLink>
+      </ul>
+    </div>
+    <div className="nav_list_user">User</div>
   </nav>
 );
 
