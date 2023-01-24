5ddf75dfe32015c5af203110c26db8f9de45ac10
gijoe2day <gjuliao32@hotmail.com>
2023-01-23 19:02:07 -0500

    Started form styling

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 const Form = () => (
-  <div>
-    <form>
+  <div className="book_form">
+    <h3>Add Book</h3>
+    <form className="book_form_inputs">
       <input name="title" type="text" placeholder="Book title" required />
       <input name="author" type="text" placeholder="Book Author" required />
+      <button type="submit">ADD BOOK</button>
     </form>
   </div>
 );
