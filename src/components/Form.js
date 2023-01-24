22d28234bb06a49ba7b155fdca7fcab41810b451
gijoe2day <gjuliao32@hotmail.com>
2023-01-24 17:54:56 -0500

    Added initial styles to form

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Form = () => (
   <div className="book_form">
-    <h3>Add Book</h3>
+    <h3>ADD NEW BOOK</h3>
     <form className="book_form_inputs">
-      <input name="title" type="text" placeholder="Book title" required />
-      <input name="author" type="text" placeholder="Book Author" required />
+      <input className="book_form_inputs_title" name="title" type="text" placeholder="Book title" required />
+      <input className="book_form_inputs_category" name="author" type="text" placeholder="Book Author" required />
       <button type="submit">ADD BOOK</button>
     </form>
   </div>
